2a07f5e2a289c42873c79beba5c2733150b7283d
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-09-06 15:33:17 +0530

    added fetch api and displayed news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
-
+import noImage from '../images/noImage.jpg'
 export class NewsItem extends Component {
     render() {
+        let {title, description, imageUrl, newsUrl} = this.props;
         return (
-            <div>
-                
+            <div className="my-3">
+                <div className="card" style={{width:"18rem"}}>
+                    <img src={imageUrl ? imageUrl : noImage} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href={newsUrl} target="_blank"className="btn btn-sm btn-primary">Read More</a>
+                    </div>
+                </div>
             </div>
         )
     }
